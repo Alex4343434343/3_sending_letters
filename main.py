69644c2ee9ec95257fa971d64c309ee31f040cba
Alex4343434343 <alexkiprip@gmail.com>
2026-01-07 15:47:33 +0300

    Добавлена переменная "email"

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ password = os.getenv('PASSWORD')
 website = 'https://dvmn.org/profession-ref-program/alexkipr/X6TXA/'
 friend_name = 'Александр'
 my_name = 'Алексей'
+email = 'alextestki@yandex.ru'
 
-letter = '''From: alextestki@yandex.ru
-To: alextestki@yandex.ru
+letter = '''From: {email}
+To: {email}
 Subject: Приглашение!
 Content-Type: text/plain; charset="UTF-8";\n\nПривет, {friend_name}! {my_name} приглашает тебя на сайт {website}!
 
@@ -27,7 +28,7 @@ Content-Type: text/plain; charset="UTF-8";\n\nПривет, {friend_name}! {my_n
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
 Регистрируйся → {website}  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.'''.format(friend_name=friend_name, my_name=my_name, website=website)
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.'''.format(email=email, friend_name=friend_name, my_name=my_name, website=website)
 letter = letter.encode('UTF-8')
 
 server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
